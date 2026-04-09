857e6642f20e175e981912c648f22932b555b49b
Patipatpatlom <patipatpatlom@gmail.com>
2026-04-09 17:24:40 +0700

    feature/login/register

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
-import React from 'react'
+import AppRouter from './router/AppRouter'
 
-function App() {
-  return (
-    <div>App</div>
-  )
+export default function App() {
+  return <AppRouter />
 }
-
-export default App
